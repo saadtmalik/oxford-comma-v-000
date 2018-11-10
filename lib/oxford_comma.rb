f5d63a255be3c51c9cd36fbd89d6acfd1ba7be07
Saad Malik <saad.t.malik@gmail.com>
2018-11-10 20:08:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    return array.join(" and ")
+  elsif array.length == 1
+    return array.join(" ")
+  elsif array.length > 2
+    array[-1].prepend "and "
+    return array.join (", ")
+  end
 end
